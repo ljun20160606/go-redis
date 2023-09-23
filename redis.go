fa246c13855ce7cc7e82a84dc7149e5ea6b0059f
ljun20160606 <ljun20160606@gmail.com>
2023-09-23 08:08:08 +0800

    revert: remove DisableIndentity

--- redis.go
+++ redis.go
@@ -301,14 +301,6 @@ func (c *baseClient) initConn(ctx context.Context, cn *pool.Conn) error {
 			return err
 		}
 	}
-	if !c.opt.DisableIndentity {
-		libName := ""
-		libVer := Version()
-		libInfo := LibraryInfo{LibName: &libName}
-		conn.ClientSetInfo(ctx, libInfo)
-		libInfo = LibraryInfo{LibVer: &libVer}
-		conn.ClientSetInfo(ctx, libInfo)
-	}
 	_, err := conn.Pipelined(ctx, func(pipe Pipeliner) error {
 		if !auth && password != "" {
 			if username != "" {
